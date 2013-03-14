15f44f534916063738f797dc32d21c3aa340092f
John S. Ryan <jtigger@infosysengr.com>
2013-03-14 07:08:35 -0700

    Resetting from POC: Base Express Project
    (what I really should do here is go back and cut a branch for the POC, but since there's only one committer at this time, I'm cool with just reverting changes.)
    The point here is to start over and to do this Test-Driven.

--- app.js
+++ app.js
@@ -3,26 +3,23 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , util = require('util')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , twitter = require('./routes/twitter')
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
 
 var app = express();
 
-app.configure(function(){
+app.configure("development", function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
-  app.use(express.cookieParser());
-  app.use(express.session( { secret : 'mykeygoeshere' }));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -32,14 +29,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/auth/twitter', twitter.auth);
-app.get('/auth/twitter/callback', twitter.auth_return);
-// app.get('/auth/twitter', function(req,res) {
-//   res.render('twitter_auth');
-// });
-// app.get('/auth/twitter/callback', function(req, res) {
-//   res.render('auth_success', { title: 'Good Buzz Hub' });
-// });
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Good Buzz Hub' });
+  res.render('index', { title: 'Express' });
 };
